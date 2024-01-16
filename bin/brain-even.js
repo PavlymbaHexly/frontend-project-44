2c32e87049b341cd50905a6f5d4c7678da487498
PavlymbaHexly <pavlymba@gmail.com>
2024-01-16 14:06:56 +0300

    projectrazum

---
+++ bin/brain-even.js
@@ -0,0 +1,32 @@
+import readlineSync from 'readline-sync';
+
+const isEven = (num) => num % 2 === 0;
+
+const playGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  let correctAnswersCount = 0;
+
+  while (correctAnswersCount < 3) {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(`Question: ${randomNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if ((isEven(randomNumber) && userAnswer.toLowerCase() === 'yes')
+      || (!isEven(randomNumber) && userAnswer.toLowerCase() === 'no')) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isEven(randomNumber) ? 'yes' : 'no'}'.`);
+      console.log(`Let's try again, ${playerName}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${playerName}!`);
+};
+
+playGame();
