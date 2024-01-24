f49efc2e8edee5e8ccbee3d756f1c6d60939748e
PavlymbaHexly <pavlymba@gmail.com>
2024-01-24 15:28:48 +0300

    project

---
+++ bin/brain-prime.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import {
+  gameStart, duplicates, Welcome, randomNum,
+} from '../src/index.js';
+
+const max = 50;
+const repeats = 3;
+
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// eslint-disable-next-line consistent-return
+function brainPrime(name) {
+  const question = randomNum(1, max);
+  console.log(`Question: ${question}`);
+  const answer = readlineSync.question('Your answer: ');
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  duplicates(correctAnswer, answer, name);
+  if (correctAnswer !== answer) {
+    return 0;
+  }
+}
+
+const gameName = brainPrime;
+
+const name = Welcome();
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+gameStart(name, gameName, repeats);
