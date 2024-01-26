e41a9a399e8c420464e60982aea79aab2c6e1786
PavlymbaHexly <pavlymba@gmail.com>
2024-01-26 14:51:10 +0300

    project

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,22 +6,30 @@ function isEven(num) {
   return num % 2 === 0;
 }
 
+const getUserAnswer = () => readlineSync.question('Your answer: ').toLowerCase();
+
+const checkAnswer = (randomNumber, userAnswer, playerName) => {
+  if ((isEven(randomNumber) && userAnswer === 'yes') || (!isEven(randomNumber) && userAnswer === 'no')) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`${userAnswer} is wrong answer`);
+  console.log(`Let's try again, ${playerName}!`);
+  return false;
+};
+
 const playGame = () => {
   const playerName = Welcome();
-
   let correctAnswersCount = 0;
 
   while (correctAnswersCount < 3) {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     console.log(`Question: ${randomNumber}`);
-    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+    const userAnswer = getUserAnswer();
 
-    if ((isEven(randomNumber) && userAnswer === 'yes') || (!isEven(randomNumber) && userAnswer === 'no')) {
-      console.log('Correct!');
+    if (checkAnswer(randomNumber, userAnswer, playerName)) {
       correctAnswersCount += 1;
     } else {
-      console.log(`${userAnswer} is wrong answer`);
-      console.log(`Let's try again, ${playerName}!`);
       return;
     }
   }
