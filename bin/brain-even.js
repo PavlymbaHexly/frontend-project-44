a7e6b36bbec52b5f7762095bf0a085ffdaeb112c
PavlymbaHexly <pavlymba@gmail.com>
2024-01-26 14:14:20 +0300

    project

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,46 +6,19 @@ function isEven(num) {
 }
 
 const playGame = () => {
-  // eslint-disable-next-line no-use-before-define
-  printWelcomeMessage();
-  // eslint-disable-next-line no-use-before-define
-  const playerName = getPlayerName();
-  // eslint-disable-next-line no-use-before-define
-  greetPlayer(playerName);
-  // eslint-disable-next-line no-use-before-define
-  printInstructions();
-  // eslint-disable-next-line no-use-before-define
-  startGame(playerName);
-};
-
-const printWelcomeMessage = () => {
   console.log('Welcome to the Brain Games!');
-};
-
-const getPlayerName = () => {
   const playerName = readlineSync.question('May I have your name? ');
-  return playerName;
-};
-
-const greetPlayer = (playerName) => {
   console.log(`Hello, ${playerName}!`);
-};
-
-const printInstructions = () => {
-  // Реализация функции вывода инструкций
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-};
 
-const startGame = (playerName) => {
-  // Реализация логики игры
   let correctAnswersCount = 0;
 
   while (correctAnswersCount < 3) {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     console.log(`Question: ${randomNumber}`);
-    const userAnswer = readlineSync.question('Your answer: ');
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
 
-    if ((isEven(randomNumber) && userAnswer.toLowerCase() === 'yes') || (!isEven(randomNumber) && userAnswer.toLowerCase() === 'no')) {
+    if ((isEven(randomNumber) && userAnswer === 'yes') || (!isEven(randomNumber) && userAnswer === 'no')) {
       console.log('Correct!');
       correctAnswersCount += 1;
     } else {
