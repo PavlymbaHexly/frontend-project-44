18ee54dcb9fe3932236a54b44a3ee7c67db68899
PavlymbaHexly <pavlymba@gmail.com>
2024-01-26 13:34:31 +0300

    project

--- src/index.js
+++ src/index.js
@@ -31,3 +31,16 @@ export function Welcome() {
 export function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
+
+// eslint-disable-next-line consistent-return
+export function askQuestionAndCheckAnswer(name, isCorrect) {
+  // eslint-disable-next-line no-undef
+  const question = randomNum(1, max);
+  console.log(`Question: ${question}`);
+  const answer = readlineSync.question('Your answer: ');
+  const correctAnswer = isCorrect(question) ? 'yes' : 'no';
+  duplicates(correctAnswer, answer, name);
+  if (correctAnswer !== answer) {
+    return 0;
+  }
+}
