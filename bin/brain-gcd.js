5b7a5509b2f0dac6d2c307ea750936edad35ca41
PavlymbaHexly <pavlymba@gmail.com>
2024-01-26 14:30:33 +0300

    project

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import {
+  // eslint-disable-next-line import/named
   gameStart, duplicates, Welcome, randomNum,
 } from '../src/index.js';
 
