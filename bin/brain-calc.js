741637a85c3f2cbda9b827566da6f62c33400fce
PavlymbaHexly <pavlymba@gmail.com>
2024-03-20 17:35:44 +0300

    project

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -2,31 +2,41 @@
 import readlineSync from 'readline-sync';
 import { Welcome } from '../src/cli.js';
 import {
-  gameStart, randomNum,
+  gameStart, duplicates, randomNum,
 } from '../src/index.js';
 
-// Функция с заданием
-function brainCalcTask() {
-  const operators = ['+', '-', '*'];
-  const max = 150;
+const operators = ['+', '-', '*'];
+const max = 150;
+const repeats = 3;
+
+// eslint-disable-next-line consistent-return
+function brainCalc(name) {
   const [random1, random2] = [randomNum(0, max), randomNum(0, max)];
   const operator = randomNum(0, operators.length - 1);
-  const expression = `${random1} ${operators[operator]} ${random2}`;
-  // eslint-disable-next-line no-eval
-  const correctAnswer = eval(expression);
-  return { question: expression, correctAnswer };
-}
-
-// Движок
-function brainCalc() {
-  const task = brainCalcTask();
-  console.log(`Question: ${task.question}`);
-  const userAnswer = parseInt(readlineSync.question('Your answer: '), 10);
-  return { ...task, userAnswer };
+  console.log(`Question: ${random1} ${operators[operator]} ${random2}`);
+  let correctAnswer = 0;
+  switch (operator) {
+    case 0:
+      correctAnswer = random1 + random2;
+      break;
+    case 1:
+      correctAnswer = random1 - random2;
+      break;
+    case 2:
+      correctAnswer = random1 * random2;
+      break;
+    default:
+      return 0;
+  }
+  const answer = parseInt(readlineSync.question('Your answer: '), 10);
+  duplicates(correctAnswer, answer, name);
+  if (correctAnswer !== answer) {
+    return 0;
+  }
 }
 
 const gameName = brainCalc;
 
 const name = Welcome();
 console.log('What is the result of the expression?');
-gameStart(name, gameName);
+gameStart(name, gameName, repeats);
