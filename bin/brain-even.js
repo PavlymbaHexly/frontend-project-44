977e53c7e978617815ef0a68e915aa1de4d39941
PavlymbaHexly <pavlymba@gmail.com>
2024-04-26 10:04:20 +0300

    last

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,41 +1,23 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import { Welcome } from '../src/cli.js';
+import { gameStart, duplicates } from '../src/index.js';
 
 function isEven(num) {
   return num % 2 === 0;
 }
 
-const getUserAnswer = () => readlineSync.question('Your answer: ').toLowerCase();
+function brainEven(name) {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  console.log(`Question: ${randomNumber}`);
+  const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+  duplicates(correctAnswer, userAnswer, name);
+  return userAnswer === correctAnswer ? 1 : 0;
+}
 
-const checkAnswer = (randomNumber, userAnswer, playerName) => {
-  if ((isEven(randomNumber) && userAnswer === 'yes') || (!isEven(randomNumber) && userAnswer === 'no')) {
-    console.log('Correct!');
-    return true;
-  }
-  console.log(`${userAnswer} is wrong answer`);
-  console.log(`Let's try again, ${playerName}!`);
-  return false;
-};
+const gameName = brainEven;
 
-const playGame = () => {
-  const playerName = Welcome();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let correctAnswersCount = 0;
-
-  while (correctAnswersCount < 3) {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    console.log(`Question: ${randomNumber}`);
-    const userAnswer = getUserAnswer();
-
-    if (checkAnswer(randomNumber, userAnswer, playerName)) {
-      correctAnswersCount += 1;
-    } else {
-      return;
-    }
-  }
-
-  console.log(`Congratulations, ${playerName}!`);
-};
-
-playGame();
+const name = Welcome();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+gameStart(name, gameName, 3);
